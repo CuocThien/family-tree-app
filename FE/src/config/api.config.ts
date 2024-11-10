cb20b7302fce42fcba3da07fca75343f7270f203
CuocThien <thiencuoc2210@gmail.com>
2024-11-10 22:42:52 +0700

    (refac) update url BE

--- FE/src/config/api.config.ts
+++ FE/src/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://family-tree.cuocthien.io.vn/api'
+  ? 'https://family-tree-be.cuocthien.io.vn/api'
   : 'http://localhost:3000/api';
 
 export const ENDPOINTS = {
