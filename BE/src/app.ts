ecb442a3e5e9f42e5fce22db80805066d4adacc2
CuocThien <thiencuoc2210@gmail.com>
2024-11-10 23:01:25 +0700

    (refac) enable CORS in prod

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(requestLogger);
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' ? 'https://family-tree.cuocthien.io.vn' : 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
